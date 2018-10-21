ca60b4165635298db2c4ac3387fae7191cc32f0e
eulersexception <bahadir.suezer@outlook.com>
2018-10-21 14:36:40 +0200

    Konto-file Aufgabe 4

---
+++ UML/Aufgabe 4/Konto.java
@@ -0,0 +1,12 @@
+public class Konto {
+
+private String bezeichnung;
+private Kunde[] zeichnungsBerechtigter;
+
+// returns balance on the account
+public Geldbetrag saldo() {}
+
+public void einzahlen(GeldBetrag einzahlung) {}
+
+}
+
