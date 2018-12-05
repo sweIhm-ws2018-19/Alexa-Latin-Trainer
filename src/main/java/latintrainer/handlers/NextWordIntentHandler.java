089e688c1524a10f85f8965c254164faa752a128
Erwin Kupris <e.kupris97@gmail.com>
2018-12-05 20:08:20 +0100

    update on highscore

--- src/main/java/latintrainer/handlers/NextWordIntentHandler.java
+++ src/main/java/latintrainer/handlers/NextWordIntentHandler.java
@@ -25,6 +25,12 @@ public class NextWordIntentHandler implements RequestHandler{
     Mode sessionMode;
     Direction sessionDir;
     public static boolean currentDirIsGerman;
+    public static boolean isFirst = true;
+    String savedMode;
+    String savedDir;
+    int savedChapter;
+    int savedHighscore;
+
     @Override
     public boolean canHandle(HandlerInput input) {
         return input.matches(intentName("NextWordIntent"));
@@ -32,52 +38,54 @@ public class NextWordIntentHandler implements RequestHandler{
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        AttributesManager attributesManager = input.getAttributesManager();
-        Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
-        persistentAttributes.put("modus", "Zufall");
-        persistentAttributes.put("richtung", "deutsch");
-        persistentAttributes.put("kapitel", 1);
-        persistentAttributes.put("highscore", 900);
-        attributesManager.setPersistentAttributes(persistentAttributes);
-        attributesManager.savePersistentAttributes();
-        String savedMode = (String) persistentAttributes.get("modus");
-        String savedDir = (String) persistentAttributes.get("richtung");
-        int savedChapter = (Integer) persistentAttributes.get("kapitel");
-        int savedHighscore = (Integer) persistentAttributes.get("highscore");
+        if (isFirst) {
+            AttributesManager attributesManager = input.getAttributesManager();
+            Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
+            persistentAttributes.put("modus", "Zufall");
+            persistentAttributes.put("richtung", "deutsch");
+            persistentAttributes.put("kapitel", 1);
+            persistentAttributes.put("highscore", 4);
+            attributesManager.setPersistentAttributes(persistentAttributes);
+            attributesManager.savePersistentAttributes();
+            savedMode = (String) persistentAttributes.get("modus");
+            savedDir = (String) persistentAttributes.get("richtung");
+            savedChapter = (Integer) persistentAttributes.get("kapitel");
+            savedHighscore = (Integer) persistentAttributes.get("highscore");
 
 
-        switch (savedMode) {
-            case "Fortschritt":
-                sessionMode = Mode.PROGRESS;
-                break;
-            case "Zufall":
-                sessionMode = Mode.CHAPTER;
-                break;
-            case "Kapitel":
-                sessionMode = Mode.RANDOM;
-                break;
-            default:
-                throw new RuntimeException();
+            switch (savedMode) {
+                case "Fortschritt":
+                    sessionMode = Mode.PROGRESS;
+                    break;
+                case "Zufall":
+                    sessionMode = Mode.CHAPTER;
+                    break;
+                case "Kapitel":
+                    sessionMode = Mode.RANDOM;
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+            switch (savedDir) {
+                case "deutsch":
+                    sessionDir = Direction.GERMAN;
+                    currentDirIsGerman = true;
+                    break;
+                case "lateinisch":
+                    sessionDir = Direction.LATIN;
+                    currentDirIsGerman = false;
+                    break;
+                case "zufall":
+                    sessionDir = Direction.RANDOM;
+                    currentDirIsGerman = Math.random() < 0.5;
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+
+            currentSession = new Session(sessionDir, sessionMode, savedChapter, savedHighscore);
+            isFirst = false;
         }
-        switch (savedDir) {
-            case "deutsch":
-                sessionDir = Direction.GERMAN;
-                currentDirIsGerman = true;
-                break;
-            case "lateinisch":
-                sessionDir = Direction.LATIN;
-                currentDirIsGerman = false;
-                break;
-            case "zufall":
-                sessionDir = Direction.RANDOM;
-                currentDirIsGerman = Math.random() < 0.5;
-                break;
-            default:
-                throw new RuntimeException();
-        }
-
-        currentSession = new Session(sessionDir, sessionMode, savedChapter, savedHighscore);
-
         // check mode
         currentQuery = WORDS.get((int) (Math.random()*20));
         // check direction
@@ -86,8 +94,6 @@ public class NextWordIntentHandler implements RequestHandler{
         String speechText = String.format("Das zu übersetzende Wort lautet %s. Bitte sage, die Antwort ist x y, " +
                 "oder wenn du es nicht weißt, keine Ahnung. Session: %s, %s, %d, %d", toTranslate, sessionDir.getDirection(),
                 sessionMode.getModeName(), savedChapter, savedHighscore);
-
-
 
         return input.getResponseBuilder()
                 .withSimpleCard("LatinTrainerSession", speechText)
