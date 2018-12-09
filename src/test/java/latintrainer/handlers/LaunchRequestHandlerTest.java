3c428fb10465aff7cce61aa514b375dd8fa3eb97
eulersexception <bahadir.suezer@outlook.com>
2018-12-09 21:40:44 +0100

    started mocking

---
+++ src/test/java/latintrainer/handlers/LaunchRequestHandlerTest.java
@@ -0,0 +1,66 @@
+package test.java.latintrainer.handlers;
+
+import com.amazon.ask.attributes.AttributesManager;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.response.ResponseBuilder;
+import main.java.latintrainer.handlers.LaunchRequestHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static main.java.latintrainer.model.LatinTrainerTools.*;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class LaunchRequestHandlerTest {
+
+    @Test
+    public void testEnabled() {
+        assertEquals(true, true);
+    }
+
+    private LaunchRequestHandler sut;
+    private HandlerInput inputMock;
+
+    @Before
+    public void setup() {
+        sut = new LaunchRequestHandler();
+        inputMock = mock(HandlerInput.class);
+    }
+
+    @Test
+    public void testCtor() {
+        assertEquals(sut.getClass(), LaunchRequestHandler.class);
+    }
+
+    @Test
+    public void testCanHandle() {
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(sut.canHandle(inputMock));
+    }
+
+    @Test
+    public void testHandle() {
+        AttributesManager attributesManager = mock(AttributesManager.class);
+        Map<String, Object> persistentAttributes = new HashMap<String, Object>();
+        persistentAttributes.put(MODE, new HashMap<String, Object>());
+
+        when(inputMock.getAttributesManager()).thenReturn(attributesManager);
+        when(attributesManager.getPersistentAttributes()).thenReturn(persistentAttributes);
+        when(inputMock.getResponseBuilder()).thenReturn(new ResponseBuilder());
+
+        Optional<Response> response = sut.handle(inputMock);
+        assertTrue(response.isPresent());
+        assertFalse(response.get().getShouldEndSession());
+    }
+
+}
---
+++ src/test/java/latintrainer/handlers/TestUtil.java
@@ -0,0 +1,78 @@
+package test.java.latintrainer.handlers;
+
+
+import com.amazon.ask.attributes.AttributesManager;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.*;
+import com.amazon.ask.response.ResponseBuilder;
+
+import main.java.latintrainer.model.LatinTrainerTools;
+import static main.java.latintrainer.model.LatinTrainerTools.*;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class TestUtil {
+
+    public static HandlerInput mockHandlerInput(String mode,
+                                                Map<String, Object> sessionAttributes,
+                                                Map<String, Object> persistentAttributes,
+                                                Map<String, Object> requestAttributes) {
+        final AttributesManager attributesManagerMock = Mockito.mock(AttributesManager.class);
+        when(attributesManagerMock.getSessionAttributes()).thenReturn(sessionAttributes);
+        when(attributesManagerMock.getPersistentAttributes()).thenReturn(persistentAttributes);
+        when(attributesManagerMock.getRequestAttributes()).thenReturn(requestAttributes);
+
+        // Mock Slots
+        final RequestEnvelope requestEnvelopeMock = RequestEnvelope.builder()
+                .withRequest(IntentRequest.builder()
+                        .withIntent(Intent.builder()
+                                .putSlotsItem(MODE_SLOT, Slot.builder()
+                                        .withName(MODE_SLOT)
+                                        .withValue(mode)
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+
+
+        // Mock Handler input attributes
+        final HandlerInput input = Mockito.mock(HandlerInput.class);
+        when(input.getAttributesManager()).thenReturn(attributesManagerMock);
+        when(input.getResponseBuilder()).thenReturn(new ResponseBuilder());
+        when(input.getRequestEnvelope()).thenReturn(requestEnvelopeMock);
+
+        return input;
+    }
+
+    public static Response standardTestForHandle(RequestHandler handler) {
+        final Map<String, Object> sessionAttributes = new HashMap<>();
+        final Map<String, Object> persistentAttributes = new HashMap<>();
+        sessionAttributes.put(MODE, "Test");
+        final HandlerInput inputMock = TestUtil.mockHandlerInput(null, sessionAttributes, persistentAttributes, null);
+        final Optional<Response> res = handler.handle(inputMock);
+
+        assertTrue(res.isPresent());
+        final Response response = res.get();
+
+        //assertFalse(response.getShouldEndSession());
+        assertNotEquals("Test", response.getReprompt());
+        assertNotNull(response.getOutputSpeech());
+        return response;
+    }
+    public static Response sessionEndedTestForHandle(RequestHandler handler) {
+        final HandlerInput inputMock = TestUtil.mockHandlerInput(null, null, null, null);
+        final Optional<Response> res = handler.handle(inputMock);
+
+        assertTrue(res.isPresent());
+        final Response response = res.get();
+        assertTrue(response.getShouldEndSession());
+        return response;
+    }
+}
